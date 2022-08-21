21c5c8de0fe99ea362f1917e8b063f752dd0874f
unknown <irawananggi76@gmail.com>
2022-08-21 18:58:14 +0700

    init spring cloud discovery services

---
+++ spring-cloud-discovery-service/src/test/java/com/anggi/irawan/spring/cloud/discovery/service/SpringCloudDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anggi.irawan.spring.cloud.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
